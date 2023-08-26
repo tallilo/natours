1fca10a32cb1faff2d8261bb2b9cb1dfd39327c4
tallilo <tallilo5011323@gmail.com>
2023-08-26 18:46:49 +0300

    adding heroku config3

--- controllers/authController.js
+++ controllers/authController.js
@@ -38,8 +38,7 @@ const createSendToken = (user, statusCode, req, res) => {
 
 exports.signup = catchAsync(async (req, res) => {
   const newUser = await User.create({
-    name: r
-    req.body.name,
+    name: req.body.name,
     email: req.body.email,
     password: req.body.password,
     passwordConfirm: req.body.passwordConfirm,
